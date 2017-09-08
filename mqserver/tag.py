795df3e28b5539d4ee98222c926f25284bf7373d
Ying Xie <yingxie3@gmail.com>
2017-09-08 14:47:30 -0700

    Add tag.py

---
+++ mqserver/tag.py
@@ -0,0 +1,96 @@
+import pdb
+import argparse
+import json
+import logging
+from vocabulary import Vocabulary
+
+from ruamel.yaml import YAML
+from pathlib import Path
+from math import log
+from spacy.en import English
+
+def load_yaml(filename):
+    yaml = YAML()
+    yamlPath = Path(filename)
+    if not yamlPath.exists():
+        return None
+    doc = yaml.load(yamlPath)
+    return doc
+
+class Definition(object):
+    def __init__(self, name, swagger):
+        self.name = name
+        orig_properties = swagger.get_properties(swagger.doc['definitions'][name])
+        self.properties = dict()
+
+        for orig_prop in orig_properties:
+            norm_prop = swagger.vocab.normalize_name(orig_prop)
+            self.properties[norm_prop] = orig_prop
+
+class SwaggerDoc(object):
+    def __init__(self, filename):
+        self.vocab = Vocabulary()
+        self.doc = load_yaml(filename)
+        self.definitions = dict()  # holds the object definitions.
+        self.parser = English()
+        self.logger = logging.getLogger(name='meqa')
+
+    # get the properties (set) from the schema
+    def get_properties(self, schema):
+        if 'allOf' in schema:
+            properties = set()
+            for s in schema['allOf']:
+                properties = properties.union(self.get_properties(s))
+            return properties
+
+        if '$ref' in schema:
+            refstr = schema['$ref']
+            # we only handle local refs for now
+            if refstr[0] != '#':
+                return set()
+
+            reflist = refstr.split('/')
+            refschema = self.doc[reflist[1]][reflist[2]]
+            return self.get_properties(refschema)
+
+        properties = set()
+        if 'properties' in schema:
+            for pname in schema['properties']:
+                properties.add(pname)
+        return properties
+
+    def normalize_name(self, name):
+        phrase = self.vocab.add_word(name)
+        tokens = self.parser(phrase)
+        return " ".join(tokens.lemma_)
+
+    def gather_words(self):
+        # we have to do two passes. First time we add all the words into the vocabulary. The second
+        # pass we may use the new vocabulary to break down some words that may not be breakable before.
+        for name, obj in self.doc['definitions'].items():
+            self.vocab.add_word(name)
+            if 'properties' in obj:
+                for key in obj['properties']:
+                    self.vocab.add_word(key)
+
+        # second pass, we lemmarize the unit words and use them as key
+        for name in self.doc['definitions']:
+            self.definitions[self.normalize_name(name)] = Definition(name, self)
+
+def main():
+    logger = logging.getLogger(name='meqa')
+    parser = argparse.ArgumentParser(description='generate tag.yaml from swagger.yaml')
+    parser.add_argument("-i", "--input", help="the swagger.yaml file", default="./meqa_data/swagger.yaml")
+    parser.add_argument("-o", "--output", help="the output file name", default="./meqa_data/tag.yaml")
+    args = parser.parse_args()
+
+    swagger = SwaggerDoc(args.input)
+    if swagger.doc == None:
+        logger.error("Failed to load file %s", args.input)
+        return
+
+    swagger.gather_words()
+
+if __name__ == '__main__':
+    #pdb.set_trace()
+    main()
---
+++ mqserver/vocabulary.py
@@ -0,0 +1,45 @@
+from math import log
+
+class Vocabulary(object):
+    def __init__(self):
+        self.words = open("words.txt").read().split()
+        self.wordcost = dict((k, log((i + 1) * log(len(self.words)))) for i, k in enumerate(self.words))
+        self.maxword = max(len(x) for x in self.words)
+
+    def infer_spaces(self, s):
+        """Uses dynamic programming to infer the location of spaces in a string
+        without spaces."""
+
+        # Find the best match for the i first characters, assuming cost has
+        # been built for the i-1 first characters.
+        # Returns a pair (match_cost, match_length).
+        def best_match(i):
+            candidates = enumerate(reversed(cost[max(0, i - self.maxword):i]))
+            return min((c + self.wordcost.get(s[i - k - 1:i], 1e99), k + 1) for k, c in candidates)
+
+        # Build the cost array.
+        cost = [0]
+        for i in range(1, len(s) + 1):
+            c, k = best_match(i)
+            cost.append(c)
+
+        # Backtrack to recover the minimal-cost string.
+        out = []
+        i = len(s)
+        while i > 0:
+            c, k = best_match(i)
+            assert c == cost[i]
+            out.append(s[i - k:i])
+            i -= k
+
+        return reversed(out)
+
+    # add a new word, return the properly broken down word
+    def add_word(self, new_word):
+        # we always treat the new words' cost as zero, since these are the words that exist in our swagger.yaml.
+        individual_words = self.infer_spaces(new_word)
+        for w in individual_words:
+            self.wordcost[w] = 0
+            self.maxword = max(self.maxword, len(w))
+
+        return " ".join(individual_words)
