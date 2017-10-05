315cb68279948b852fb674223eadbcda51fa6698
Ying Xie <yingxie3@gmail.com>
2017-10-04 22:39:03 -0700

    minor fixes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-VERSION="0.8"
-docker build -f Dockerfile.server-base -t meqa/python:$VERSION .
-docker build -f Dockerfile.server -t yingxie3/mqserver:$VERSION .
+
+docker build -f Dockerfile.server-base -t meqa/python .
+docker build -f Dockerfile.server -t yingxie3/mqserver .
