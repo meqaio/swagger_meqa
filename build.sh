85c65bd0842d3797e9c207f9e5a52e9eda000118
Ying Xie <ying@meqa.io>
2017-10-06 14:32:29 -0700

    Clean up repo, remove non-essential files.

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-docker build -f Dockerfile.server-base -t meqa/python .
-docker build -f Dockerfile.server -t yingxie3/mqserver:latest .
-docker build -f Dockerfile.client -t meqa/go:latest .
