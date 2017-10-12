0c61af46003b37af2d8643a949517a062348ffde
Ying Xie <ying@meqa.io>
2017-10-11 23:25:02 -0700

    move the Dockerfiles

--- build-docker.sh
+++ build-docker.sh
@@ -7,8 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 echo "building meqa docker images..."
-#docker build -f docker/Dockerfile.server-base -t meqa/python:latest .
-#docker build -f docker/Dockerfile.server -t meqa/mqserver:latest .
-docker build -f docker/Dockerfile.client -t meqa/go:latest .
+#docker build -f mqserver/Dockerfile.base -t meqa/python:latest .
+docker build -f mqserver/Dockerfile -t meqa/mqserver:latest .
+docker build -f mqgo/Dockerfile -t meqa/go:latest .
 
 popd
