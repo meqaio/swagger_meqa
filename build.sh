18ea331d6dcf8e965fb5b2185ee79675595a3120
Ying Xie <ying@meqa.io>
2017-10-12 10:43:58 -0700

    clean up Dockerfile

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
+echo "building mqgo directory..."
+mqgo/build-vendor.sh
+
 echo "building meqa docker images..."
-#docker build -f mqserver/Dockerfile.base -t meqa/python:latest .
-docker build -f mqserver/Dockerfile -t meqa/mqserver:latest .
+docker build -f mqserver/Dockerfile -t meqa/python:latest .
 docker build -f mqgo/Dockerfile -t meqa/go:latest .
 
 popd
